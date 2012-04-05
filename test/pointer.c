cdec20c415f97554dc57505c84b6f3bd486821ee
Rui Ueyama <rui314@gmail.com>
2012-04-04 22:21:18 -0700

    Add tests for pointer dereference.

--- test/pointer.c
+++ test/pointer.c
@@ -28,6 +28,22 @@ void t5() {
     expect(65, *s);
 }
 
+void t6() {
+    struct tag {
+        int val;
+        struct tag *next;
+    };
+    struct tag node1 = { 1, NULL };
+    struct tag node2 = { 2, &node1 };
+    struct tag node3 = { 3, &node2 };
+    struct tag *p = &node3;
+    expect(3, p->val);
+    expect(2, p->next->val);
+    expect(1, p->next->next->val);
+    p->next = p->next->next;
+    expect(1, p->next->val);
+}
+
 void testmain() {
     print("pointer");
     t1();
@@ -35,4 +51,5 @@ void testmain() {
     t3();
     t4();
     t5();
+    t6();
 }
