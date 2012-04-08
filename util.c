f1a707154a9651fcaf79ed9d0c4e342aa76ba68f
Rui Ueyama <rui314@gmail.com>
2012-04-07 23:21:38 -0700

    Display error message in red.

--- util.c
+++ util.c
@@ -1,12 +1,13 @@
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "8cc.h"
 
 bool suppress_warning = false;
 
 void errorf(char *file, int line, char *fmt, ...) {
-    fprintf(stderr, "%s:%d: ", file, line);
-    fprintf(stderr, "%s: ", input_position());
+    fprintf(stderr, isatty(fileno(stderr)) ? "\e[1;31m[ERROR]\e[0m " : "[ERROR] ");
+    fprintf(stderr, "%s:%d: %s: ", file, line, input_position());
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
@@ -18,8 +19,8 @@ void errorf(char *file, int line, char *fmt, ...) {
 void warn(char *fmt, ...) {
     if (suppress_warning)
         return;
+    fprintf(stderr, isatty(fileno(stderr)) ? "\e[1;31m[WARNING]\e[0m " : "[WARNING] ");
     fprintf(stderr, "%s: ", input_position());
-    fprintf(stderr, "warning: ");
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
