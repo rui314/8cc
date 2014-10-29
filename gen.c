5af1f6d5a8ae458a84c883b6f96913f8d68a3221
Rui Ueyama <rui314@gmail.com>
2014-10-29 00:49:40 -0700

    Use early return.

--- gen.c
+++ gen.c
@@ -820,25 +820,24 @@ static void emit_gvar(Node *node) {
 
 static bool maybe_emit_builtin(Node *node) {
     SAVE;
-    if (!strcmp("__builtin_return_address", node->fname)) {
-        push("r11");
-        assert(vec_len(node->args) == 1);
-        emit_expr(vec_head(node->args));
-        char *loop = make_label();
-        char *end = make_label();
-        emit("mov %%rbp, %%r11");
-        emit_label(loop);
-        emit("test %%rax, %%rax");
-        emit("jz %s", end);
-        emit("mov (%%r11), %%r11");
-        emit("sub $1, %%rax");
-        emit_jmp(loop);
-        emit_label(end);
-        emit("mov 8(%%r11), %%rax");
-        pop("r11");
-        return true;
-    }
-    return false;
+    if (strcmp("__builtin_return_address", node->fname))
+        return false;
+    push("r11");
+    assert(vec_len(node->args) == 1);
+    emit_expr(vec_head(node->args));
+    char *loop = make_label();
+    char *end = make_label();
+    emit("mov %%rbp, %%r11");
+    emit_label(loop);
+    emit("test %%rax, %%rax");
+    emit("jz %s", end);
+    emit("mov (%%r11), %%r11");
+    emit("sub $1, %%rax");
+    emit_jmp(loop);
+    emit_label(end);
+    emit("mov 8(%%r11), %%rax");
+    pop("r11");
+    return true;
 }
 
 static void classify_args(Vector *ints, Vector *floats, Vector *rest, Vector *args) {
