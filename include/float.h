8a60dfd053ba70272a75c6647339f196ee1290b1
Rui Ueyama <rui314@gmail.com>
2014-11-03 20:22:22 -0800

    Add float.h.
    The long double precisions are less accurate than they should
    be on x86-64. This is becuase of the lack of long double support.
    Currently long double is the same type as double. It'll be fixed
    in a following patch.

---
+++ include/float.h
@@ -0,0 +1,40 @@
+// Copyright 2014 Rui Ueyama <rui314@gmail.com>
+// This program is free software licensed under the MIT license.
+
+#ifndef __STDFLOAT_H
+#define __STDFLOAT_H
+
+#define FLT_RADIX 2
+#define FLT_ROUNDS 1
+
+#define FLT_DIG 6
+#define FLT_EPSILON 0x1p-23
+#define FLT_MANT_DIG 24
+#define FLT_MAX 0x1.fffffep+127
+#define FLT_MAX_10_EXP 38
+#define FLT_MAX_EXP 128
+#define FLT_MIN 0x1p-126
+#define FLT_MIN_10_EXP -37
+#define FLT_MIN_EXP -125
+
+#define DBL_DIG 15
+#define DBL_EPSILON 0x1p-52
+#define DBL_MANT_DIG 53
+#define DBL_MAX 0x1.fffffffffffffp+1023
+#define DBL_MAX_10_EXP 308
+#define DBL_MAX_EXP 1024
+#define DBL_MIN 0x1p-1022
+#define DBL_MIN_10_EXP -307
+#define DBL_MIN_EXP -1021
+
+#define LDBL_DIG 15
+#define LDBL_EPSILON 0x1p-52
+#define LDBL_MANT_DIG 53
+#define LDBL_MAX 0x1.fffffffffffffp+1023
+#define LDBL_MAX_10_EXP 308
+#define LDBL_MAX_EXP 1024
+#define LDBL_MIN 0x1p-1022
+#define LDBL_MIN_10_EXP -307
+#define LDBL_MIN_EXP -1021
+
+#endif
--- test/float.c
+++ test/float.c
@@ -1,6 +1,8 @@
 // Copyright 2012 Rui Ueyama <rui314@gmail.com>
 // This program is free software licensed under the MIT license.
 
+#include <float.h>
+#include <stdarg.h>
 #include "test.h"
 
 float tf1(float a)  { return a; }
@@ -16,8 +18,56 @@ double recursive(double a) {
     return recursive(3.33);
 }
 
+char *fmt(char *fmt, ...) {
+    static char buf[128];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+    return buf;
+}
+
+char *fmtint(int x) { return fmt("%d", x); }
+char *fmtdbl(double x) { return fmt("%a", x); }
+
+void std() {
+    expect_string("2", fmtint(FLT_RADIX));
+    expect_string("1", fmtint(FLT_ROUNDS));
+
+    expect_string("6", fmtint(FLT_DIG));
+    expect_string("0x1p-23", fmtdbl(FLT_EPSILON));
+    expect_string("24", fmtint(FLT_MANT_DIG));
+    expect_string("0x1.fffffep+127", fmtdbl(FLT_MAX));
+    expect_string("38", fmtint(FLT_MAX_10_EXP));
+    expect_string("128", fmtint(FLT_MAX_EXP));
+    expect_string("0x1p-126", fmtdbl(FLT_MIN));
+    expect_string("-37", fmtint(FLT_MIN_10_EXP));
+    expect_string("-125", fmtint(FLT_MIN_EXP));
+
+    expect_string("15", fmtint(DBL_DIG));
+    expect_string("0x1p-52", fmtdbl(DBL_EPSILON));
+    expect_string("53", fmtint(DBL_MANT_DIG));
+    expect_string("0x1.fffffffffffffp+1023", fmtdbl(DBL_MAX));
+    expect_string("308", fmtint(DBL_MAX_10_EXP));
+    expect_string("1024", fmtint(DBL_MAX_EXP));
+    expect_string("0x1p-1022", fmtdbl(DBL_MIN));
+    expect_string("-307", fmtint(DBL_MIN_10_EXP));
+    expect_string("-1021", fmtint(DBL_MIN_EXP));
+
+    expect_string("15", fmtint(LDBL_DIG));
+    expect_string("0x1p-52", fmtdbl(LDBL_EPSILON));
+    expect_string("53", fmtint(LDBL_MANT_DIG));
+    expect_string("0x1.fffffffffffffp+1023", fmtdbl(LDBL_MAX));
+    expect_string("308", fmtint(LDBL_MAX_10_EXP));
+    expect_string("1024", fmtint(LDBL_MAX_EXP));
+    expect_string("0x1p-1022", fmtdbl(LDBL_MIN));
+    expect_string("-307", fmtint(LDBL_MIN_10_EXP));
+    expect_string("-1021", fmtint(LDBL_MIN_EXP));
+}
+
 void testmain(void) {
     print("float");
+    std();
 
     expect(0.7, .7);
     float v1 = 10.0;
