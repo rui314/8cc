a7fcab057c901f0cf004528de5274e1a16690a45
Rui Ueyama <rui314@gmail.com>
2014-11-06 18:43:51 -0800

    Untabify.

--- cpp.c
+++ cpp.c
@@ -554,8 +554,8 @@ static Vector *read_intexpr_line(void) {
         if (!tok) return r;
         if (is_ident(tok, "defined"))
             vec_push(r, read_defined_op());
-	// N1570 6.10.1.4 says that remaining identifiers should be
-	// replaced with pp-number 0.
+        // N1570 6.10.1.4 says that remaining identifiers should be
+        // replaced with pp-number 0.
         else if (tok->kind == TIDENT)
             vec_push(r, cpp_token_zero);
         else
