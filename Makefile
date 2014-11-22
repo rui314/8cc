8eda72a2c1deb1edc48066553b91c815fc2dcd6a
Rui Ueyama <rui314@gmail.com>
2014-11-22 09:45:01 -0800

    Makefile: Compile with -O2.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-Wall -std=gnu11 -g -I. -O0
+CFLAGS=-Wall -Wno-strict-aliasing -std=gnu11 -g -I. -O2
 OBJS=cpp.o debug.o dict.o gen.o lex.o vector.o parse.o buffer.o map.o error.o
 TESTS := $(patsubst %.c,%.bin,$(filter-out test/testmain.c,$(wildcard test/*.c)))
 override CFLAGS += -DBUILD_DIR='"$(shell pwd)"'
--- test/builtin.c
+++ test/builtin.c
@@ -1,7 +1,12 @@
 // Copyright 2012 Rui Ueyama <rui314@gmail.com>
 // This program is free software licensed under the MIT license.
 
+// This test depends on the stack frame and code layout and does not
+// run with gcc -O2.
+
 #include "test.h"
+
+#ifdef __8cc__
 
 static void *test_return_address_sub2(void) {
     return __builtin_return_address(1);
@@ -19,6 +24,10 @@ static void test_return_address(void) {
  L2:
     expect(1, &&L1 < ptr && ptr <= &&L2);
 }
+
+#else
+static void test_return_address() {}
+#endif
 
 void testmain(void) {
     print("builtin");
