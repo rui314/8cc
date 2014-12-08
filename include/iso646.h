5cc40edb3992830c69b0e4763c6220228251f95b
Rui Ueyama <rui314@gmail.com>
2014-12-08 17:25:28 +0900

    Add iso646.h.
    Nobody is really using this, but this is still
    a part of the standard.

---
+++ include/iso646.h
@@ -0,0 +1,21 @@
+// Copyright 2014 Rui Ueyama <rui314@gmail.com>
+// This program is free software licensed under the MIT license.
+
+// C11 7.9 Alternative Spellings
+
+#ifndef __ISO646_H
+#define __ISO646_H
+
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+
+#endif
---
+++ test/iso646.c
@@ -0,0 +1,23 @@
+// Copyright 2014 Rui Ueyama <rui314@gmail.com>
+// This program is free software licensed under the MIT license.
+
+#include <iso646.h>
+#include "test.h"
+
+#define SS(x) #x
+#define S(x) SS(x)
+
+void testmain(void) {
+    print("iso646");
+    expect_string("&&", S(and));
+    expect_string("&=", S(and_eq));
+    expect_string("&", S(bitand));
+    expect_string("|", S(bitor));
+    expect_string("~", S(compl));
+    expect_string("!", S(not));
+    expect_string("!=", S(not_eq));
+    expect_string("||", S(or));
+    expect_string("|=", S(or_eq));
+    expect_string("^", S(xor));
+    expect_string("^=", S(xor_eq));
+}
