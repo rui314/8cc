rui314/8cc

commit 188b139cf5cf5fdd9d61a02692bb5b7d5a049305

Respect ty->usig in get_load_inst.
get_load_inst needs to consider the signed-ness of the type
that it's emitting a load instruction for. If the load is
unsigned, emit zero-extended load instructions.

gen.c
@@ -124,10 +124,10 @@ static char *get_int_reg(Type *ty, char r) {
 
 static char *get_load_inst(Type *ty) {
     switch (ty->size) {
-    case 1: return "movsbq";
-    case 2: return "movswq";
-    case 4: return "movslq";
-    case 8: return "mov";
+    case 1: return ty->usig ? "movzbq" : "movsbq";
+    case 2: return ty->usig ? "movzwq" : "movswq";
+    case 4: return ty->usig ? "mov" : "movslq";
+    case 8: return "movq";
     default:
         error("Unknown data size: %s: %d", ty2s(ty), ty->size);
     }

commit 257c8098d6c61d52b16431a552fd24eed1547388

Fix error in get_load_inst introduced in 188b139.
For word-length (4-byte) unsigned loads, we need:
    mov (%rax), %eax.

gen.c
@@ -122,11 +122,20 @@ static char *get_int_reg(Type *ty, char r) {
     }
 }
 
-static char *get_load_inst(Type *ty) {
+static char *get_load_inst(Type *ty, char **dest) {
+    *dest = "rax";
+
     switch (ty->size) {
     case 1: return ty->usig ? "movzbq" : "movsbq";
     case 2: return ty->usig ? "movzwq" : "movswq";
-    case 4: return ty->usig ? "mov" : "movslq";
+    case 4: {
+        if (ty->usig) {
+            *dest = "eax";
+            return "mov";
+        }
+
+        return "movslq";
+    }
     case 8: return "movq";
     default:
         error("Unknown data size: %s: %d", ty2s(ty), ty->size);
@@ -229,8 +238,9 @@ static void emit_gload(Type *ty, char *label, int off) {
             emit("lea %s(#rip), #rax", label);
         return;
     }
-    char *inst = get_load_inst(ty);
-    emit("%s %s+%d(#rip), #rax", inst, label, off);
+    char *dest;
+    char *inst = get_load_inst(ty, &dest);
+    emit("%s %s+%d(#rip), #%s", inst, label, off, dest);
     maybe_emit_bitshift_load(ty);
 }
 
@@ -269,8 +279,9 @@ static void emit_lload(Type *ty, char *base, int off) {
     } else if (ty->kind == KIND_DOUBLE || ty->kind == KIND_LDOUBLE) {
         emit("movsd %d(#%s), #xmm0", off, base);
     } else {
-        char *inst = get_load_inst(ty);
-        emit("%s %d(#%s), #rax", inst, off, base);
+        char *dest;
+        char *inst = get_load_inst(ty, &dest);
+        emit("%s %d(#%s), #%s", inst, off, base, dest);
         maybe_emit_bitshift_load(ty);
     }
 }